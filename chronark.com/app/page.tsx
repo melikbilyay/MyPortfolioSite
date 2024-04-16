e35e75ad26df1a3931c1a70d879d3f65725bbb45
melikbilyay <37048509+melikbilyay@users.noreply.github.com>
2024-04-16 17:59:42 +0300

    Update page.tsx

--- chronark.com/app/page.tsx
+++ chronark.com/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     return (
         <div
             className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-            <img src="/Me.png" alt="Me" className="my-4 w-32 h-32"/>
+
             <nav className="my-16 animate-fade-in">
                 <ul className="flex items-center justify-center gap-4">
 
@@ -32,6 +32,7 @@ export default function Home() {
                 className="absolute inset-0 -z-10 animate-fade-in"
                 quantity={100}
             />
+            <img src="/Me.png" alt="Me" className="my-4 w-40 h-40"/>
             <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
                 Necip Melik Bilyay
             </h1>
